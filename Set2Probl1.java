d405c2b53bb4e978a9c986a2bd1167730f0b98e5
dhars-03 <165540664+dhars-03@users.noreply.github.com>
2025-02-23 10:23:59 +0530

    Write a program which takes two values x and y. Prints x for y number of times.

--- Set2Probl1.java
+++ Set2Probl1.java
@@ -1,12 +1,13 @@
-public class Set2Problem1
+import java.util.Scanner;
+public class Set2Prob1
 {
 	public static void main(String[] args) {
 	    Scanner obj=new Scanner(System.in);
-      System.out.println("Enter x: ");
-      System.out.println("Enter y: ");
+            System.out.println("Enter x: ");
+            System.out.println("Enter y: ");
 	    int x=obj.nextInt();
 	    int y=obj.nextInt();
-	    for (int i=x; i<=y;i++)
+	    for (int i=1;i<=y;i++)
 	    System.out.println(x);
 	}
 }
