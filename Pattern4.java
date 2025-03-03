947fb1928949b1b201f81928ec78de846249c026
dhars-03 <165540664+dhars-03@users.noreply.github.com>
2025-03-03 22:35:19 +0530

    Given a number N. The task is to print a series of * from 1 till N term.  Example :  Input: N = 3  Output: * ** ***

---
+++ Pattern4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Pattern4{
+public static void main(String[] args){
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+    }System.out.print(" ");
+}
+}
+}
