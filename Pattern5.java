9f6344876e21570598818e8ed45c376f2014e269
dhars-03 <165540664+dhars-03@users.noreply.github.com>
2025-03-03 23:03:26 +0530

    The task is to print a series from 1 till N in upside down order .  Example:  Input: N = 3 Output: 1 2 3 /n 2 1 /n 1

---
+++ Pattern5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Pattern5{
+  public static void main(String[] args){
+    Scanner obj=new Scanner(System.in);
+    int n=obj.nextInt();
+    for(int i=1;i<=n;i++){
+           for(int j=1;j<=n-i+1;j++){
+               System.out.print(j);
+           }
+           System.out.println();
+       }
+    }
+}
+    
+
+
+
+
+
+
+
+
+
